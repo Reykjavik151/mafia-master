23c1514bed0f6be7713ae378f36a06a289605a3e
Roman Andrieiev <reykjavik151@gmail.com>
2024-08-14 23:43:18 +0100

    refactor: keyExtractor for DashboardScreen

--- src/screens/DashboardScreen/index.tsx
+++ src/screens/DashboardScreen/index.tsx
@@ -4,13 +4,16 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { DefaultBackgroundLinear, ExtendedButton, GameRecordRow, Header } from '#components';
 import { useDashboardScreenController } from './hooks/useDashboardScreenController';
 import { GameRecord } from '#models/GameRecord';
+import { useSpecificKeyExtractor } from 'react-native-hookbox';
 
 export const DashboardScreen = () => {
-  const { bottom: bottomInset, top: topInset } = useSafeAreaInsets();
+  const { bottom: bottomInset } = useSafeAreaInsets();
 
   const { gameRecords, goToNewGame } = useDashboardScreenController();
 
   const renderGameRecordItem: ListRenderItem<GameRecord> = useCallback(({ item }) => <GameRecordRow item={item} />, []);
+
+  const keyExtractor = useSpecificKeyExtractor<GameRecord>('game-record', 'id');
 
   return (
     <View className="flex-1">
@@ -23,7 +26,7 @@ export const DashboardScreen = () => {
           <FlatList
             data={[...gameRecords, ...gameRecords, ...gameRecords]}
             renderItem={renderGameRecordItem}
-            keyExtractor={(item) => item.id.toString()}
+            keyExtractor={keyExtractor}
             className="flex-1"
             contentContainerClassName="flex-1 p-6 gap-6"
           />
